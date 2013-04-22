41d26d0fe7601aceec17a47488dec9814f5fbc5f
Salvatore D'Agositno <toto19@gmail.com>
2013-04-22 19:29:38 -0400

    made content window 50px higher

--- js/load-content.js
+++ js/load-content.js
@@ -14,8 +14,8 @@ DT.fn.hideContent = function(callback) {
 		duration: 500,
 		complete: function() {
 			$(".plus-min a").removeClass("act");
-			if (callback) {  
-				callback();  
+			if (callback) {
+				callback();
 			}
 		}
 	});
@@ -25,7 +25,7 @@ DT.fn.showContent = function(delay) {
 	var delay = delay || 0;
 
 	DT.$.holder.removeClass("content-hidden").stop().delay(delay).animate({
-		"height" : 350
+		"height" : 400
 	}, {
 		duration: 500,
 		complete: function() {
@@ -58,11 +58,11 @@ DT.fn.showAdvanced = function(delay) {
 
 DT.fn.hideControls = function(delay) {
 	var delay = delay || 0;
-	
+
 	DT.$.controlsBasic.stop().delay(delay).animate({
 		"top" : DT.v.controlsBasicTop
 	}, 500);
-	
+
 	DT.$.controlsAdvanced.stop().delay(delay).animate({
 		"right" : DT.v.controlsAdvancedRight
 	}, 500);
@@ -81,11 +81,11 @@ DT.fn.toggleContent = function() {
 
 DT.fn.showAll = function(delay) {
 	var delay = delay || 0;
-	
+
 	DT.$.superslides	= $("#superslides");
 	DT.v.doContent		= DT.$.superslides.attr("data-content")	|| "auto";
 	DT.v.doControls		= DT.$.superslides.attr("data-controls") || "auto";
-	
+
 /*	console.log(DT.$.superslides)
 	console.log("content: "+DT.v.doContent)
 	console.log("controls: "+DT.v.doControls)
@@ -102,10 +102,10 @@ DT.fn.showAll = function(delay) {
 			DT.fn.showContent(delay);
 		}
 	}
-	
+
 	if (DT.v.doControls == "basic") {
 		DT.fn.showBasic(delay);
-	} 
+	}
 	else if (DT.v.doControls == "advanced"){
 		DT.fn.showAdvanced(delay);
 	} else {
@@ -146,14 +146,14 @@ DT.fn.loadContent = function() {
 }
 
 jQuery(document).ready(function($){
-	
+
 	var winHash = location.hash;
 	if (/#!\//.test(winHash)){
 		location.href = winHash.replace(/#!\//,"");
 	}
 
 	DT.fn.loadContent();
-	
+
 	DT.$.holder = $("#holder");
 
 	DT.$.controlsBasic		= $("#controls-basic");
@@ -167,13 +167,13 @@ jQuery(document).ready(function($){
 /* on Document load: begin */
 	DT.fn.showAll(1000);
 /* on Document load: end */
-	
+
 	$(".show-hide.show").on("click", function(e) {
 		e.preventDefault();
 		DT.fn.showAdvanced();
 		DT.fn.hideContent();
 	});
-	
+
 	$(".show-hide.hide").on("click", function(e) {
 		e.preventDefault();
 		DT.fn.showBasic();
@@ -184,19 +184,19 @@ jQuery(document).ready(function($){
 		e.preventDefault();
 		DT.fn.toggleContent();
 	});
-	
+
 	/* Ajax content loader: begin */
 	/* Loads a page from a "href" and pushes "#content" to "#content-holder" */
 	$("#nav:not(.ignore-ajax) ul a:not(.ignore-ajax)").click(function(e){
 		e.preventDefault();
-	
+
 		var $this = $(this),
 			page_path = $this.attr("href");
-		
+
 		$this.trigger("mouseleave");
 
 		location.hash = "!/"+page_path;
-	
+
 //		$this.parents("ul").find("li").removeClass("act");
 //		$this.parents("li").addClass("act");
 
